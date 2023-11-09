079668329edb074a3ed087f44d499ffa02578e3c
Jasmine <jasthakkar83@gmail.com>
2023-11-09 15:40:15 +0530

    adding calculator

---
+++ src/test/java/utility/BasicCalculator.java
@@ -0,0 +1,13 @@
+package utility;
+
+import org.testng.annotations.Test;
+
+@Test
+public class BasicCalculator {
+public void sum() {
+	int a = 5;
+	int b = 6;
+	int sum=a+b;
+	System.out.println("Addition of two numbers is "+sum);
+}
+}
